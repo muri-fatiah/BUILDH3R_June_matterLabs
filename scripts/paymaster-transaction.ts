3935b5ff0c2ad8f1b50763f51013b941d93d12f8
muri-fatiah <muritalafatiah@gmail.com>
2024-06-13 20:05:51 -0700

    Update paymaster-transaction.ts

--- scripts/paymaster-transaction.ts
+++ scripts/paymaster-transaction.ts
@@ -6,9 +6,9 @@ import TokenArtifact from "../artifacts/TestToken";
 import ZeekMessagesArtifact from "../artifacts/ZeekSecretMessages";
 
 // Address of the ZeekMessages contract
-const ZEEK_MESSAGES_CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+const ZEEK_MESSAGES_CONTRACT_ADDRESS = "0x05925e4821fbbf91bed310b063daae491064ffae";
 // Address of the ERC20 token contract
-const TOKEN_CONTRACT_ADDRESS = "0xf8e81D47203A594245E36C48e151709F0C19fBe8"
+const TOKEN_CONTRACT_ADDRESS = "0x36cc3fb5893bc0be799eadf6b5aa80a4dcb268bf"
 // Message to be sent to the contract
 const NEW_MESSAGE = "This tx cost me no ETH!";
 
